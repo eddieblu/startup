daf87b996355d5348a6b22c2cb54847d5d49a720
Bethany Edwards <bethany.edwards@cocooncs.com>
2025-02-12 17:59:31 -0700

    created router

--- src/app.jsx
+++ src/app.jsx
@@ -2,35 +2,52 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from './login/login';
+import { Post } from './post/post';
+import { Feed } from './feed/feed';
+
 export default function App() {
-    return <div className="body bg-dark text-light">
-        <header className="container-fluid">
-            <nav className="navbar navbar-dark">
-                <a className="navbar-brand" href="/">
-                    <h1>Scatter Sunshine</h1>
-                </a>
-                <menu className="navbar-nav">
-                    <li className="nav-item">
-                        <a className="nav-link" href="index.html">Home</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="post.html">Post</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="feed.html">Feed</a>
-                    </li>
-                </menu>
-            </nav>
-        </header>
+    return (
+        <BrowserRouter>
+            <div className="body bg-dark text-light">
+                <header className="container-fluid">
+                    <nav className="navbar navbar-dark">
+                        <NavLink className='nav-link' to='/'>
+                            <h1>Scatter Sunshine</h1>
+                        </NavLink>
+                        <menu className="navbar-nav">
+                            <li className="nav-item">
+                                <NavLink className='nav-link' to='/'>Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className='nav-link' to='post'>Post</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className='nav-link' to='feed'>Feed</NavLink>
+                            </li>
+                        </menu>
+                    </nav>
+                </header>
 
-        <main>App components go here</main>
+                <Routes>
+                    <Route path='/' element={<Login />} exact />
+                    <Route path='/post' element={<Post />} />
+                    <Route path='/feed' element={<Feed />} />
+                    <Route path='*' element={<NotFound />} />
+                </Routes>
+                <footer className="text-white">
+                    <div className="container-fluid">
+                        <span className="text-reset">Bethany Edwards</span>
+                        <a className="text-reset" href="https://github.com/eddieblu/startup">GitHub Source</a>
+                    </div>
+                </footer>
 
-        <footer className="text-white">
-            <div className="container-fluid">
-                <span className="text-reset">Bethany Edwards</span>
-                <a className="text-reset" href="https://github.com/eddieblu/startup">GitHub Source</a>
             </div>
-        </footer>
-
-    </div>;
+        </BrowserRouter>
+    );
 }
+
+function NotFound() {
+    return <main className="container-fluid bg-secondary text-center">404: Return to sender. Address unknown.</main>;
+  }
