5d369a1fd40c6abbc83a1b5b1380079463b30aa5
Bethany Edwards <bethany.edwards@cocooncs.com>
2025-02-12 17:46:28 -0700

    create view components

---
+++ src/post/post.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Post() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>post displayed here</div>
+    </main>
+  );
+}
