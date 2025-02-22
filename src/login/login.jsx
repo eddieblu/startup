e714ae3fb4646e5c91864bf78e57c933512acca0
Bethany Edwards <bethany.edwards@cocooncs.com>
2025-02-21 17:17:17 -0700

    update(login.jsx) set up jsx for public api calls

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,31 +2,44 @@ import React from 'react';
 import './login.css';
 
 export function Login() {
-  return (
-    <main className="body container-fluid text-center">
-        <div>
-            <div id="picture" className="picture-box">
-                <img width="400px" src="sunshine.jpg" alt="random" />
-            </div>
-            <p id="zen-quote">
-                Zen Quote placeholder (public API)
-            </p>
-            <h1>Welcome to Scatter Sunshine</h1>
-            <p id="instruction">
-                Login to a pre-existing account or register a new account to scatter your sunshine.
-            </p>
-        </div>
+    const [quote, setQuote] = React.useState('Loading...');
+    const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
 
-        <form method="get" action="post">
-            <div className="input-group mb-3">
-                <input className="form-control" type="text" placeholder="username" />
+    React.useEffect(() => {
+        setQuote('“Treat everyone with politeness and kindness, not because they are nice, but because you are.”');
+        setQuoteAuthor('Roy T. Bennett');
+    }, []);
+
+
+
+    return (
+        <main className="body container-fluid text-center">
+            <div>
+                <div id="picture">
+                    <img width="400px" src="sunshine.jpg" alt="random" />
+                </div>
+                <h1>Welcome to Scatter Sunshine</h1>
             </div>
-            <div className="input-group mb-3">
-                <input className="form-control" type="password" placeholder="password" />
+
+            <div>
+                <p id="instruction">
+                    Login to a pre-existing account or register a new account.
+                </p>
+                <form method="get" action="post">
+                    <div className="input-group mb-3">
+                        <input className="form-control" type="text" placeholder="username" />
+                    </div>
+                    <div className="input-group mb-3">
+                        <input className="form-control" type="password" placeholder="password" />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Login</button>
+                    <button type="submit" className="btn">Register</button>
+                </form>
             </div>
-            <button type="submit" className="btn btn-primary">Login</button>
-            <button type="submit" className="btn">Register</button>
-        </form>
-    </main>
-  );
+            <div id="zen-quote">
+                <p id="quote">{quote}</p>
+                <p id="quote-author">{quoteAuthor}</p>
+            </div>
+        </main>
+    );
 }
