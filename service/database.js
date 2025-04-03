6d15f545e45d4e0f45a23b7f7f4758f6231f52f5
BethanyEdwards <bethany.edwards@cocooncs.com>
2025-04-02 19:59:28 -0600

    temp implementation of functions in database.js

--- service/database.js
+++ service/database.js
@@ -34,9 +34,69 @@ async function updateUser(user) {
     await userCollection.updateOne({ email: user.email }, { $set: user });
 }
 
+async function incrementUserStreak(username) {
+    await userCollection.updateOne(
+        { username: username },
+        { $inc: { streak: 1 } }
+    );
+}
+
+async function addPost(post) {
+    await postCollection.insertOne(post);
+}
+
+async function getAllPosts(username) {
+    const userPost = await postCollection.findOne({ username: username });
+
+    const otherPosts = await postCollection
+        .find({ username: { $ne: username } })
+        .toArray();
+
+    if (!userPost) {
+        return otherPosts;
+    }
+    return [userPost, ...otherPosts];
+}
+
+function getPostById(postId) {
+    return postCollection.findOne({ id: postId });
+}
+
+async function updatePostContent(postId, newContent) {
+    await postCollection.updateOne(
+        { id: postId },
+        { $set: { content: newContent } }
+    );
+}
+
+async function toggleHeart(postId, username) {
+    // If user not in heartedBy, add them:
+    await postCollection.updateOne(
+        { id: postId },
+        {
+            $addToSet: { heartedBy: username },
+            $inc: { hearts: 1 }
+        }
+    );
+    // If user already in heartedBy, remove them:
+    await postCollection.updateOne(
+        { id: postId },
+        {
+            $pull: { heartedBy: username },
+            $inc: { hearts: -1 }
+        }
+    );
+}
+
 module.exports = {
     getUser,
     getUserByToken,
     addUser,
-    updateUser
+    updateUser,
+    incrementUserStreak,
+    addPost,
+    getAllPosts,
+    getPostById,
+    updatePostContent,
+    toggleHeart
 }
